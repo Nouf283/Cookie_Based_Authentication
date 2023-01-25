5287f7ae84f0e376b2d16ca61d54514a5ccb8d08
Nouf283 <noufawal0311@gmail.com>
2023-01-25 21:15:09 +0600

    first commit

---
+++ Cookie_Based_Authentication/Model/Credential.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cookie_Based_Authentication.Model
+{
+    public class Credential
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
---
+++ Cookie_Based_Authentication/Pages/Login/_Login.cshtml.cs
@@ -0,0 +1,37 @@
+using Cookie_Based_Authentication.Model;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Claims;
+
+namespace Cookie_Based_Authentication.Pages.Login
+{
+    public class _LoginModel : PageModel
+    {
+        [BindProperty]
+        public Credential Credential { get; set; }
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (!ModelState.IsValid) return Page();
+
+            if(Credential.UserName == "admin" && Credential.Password == "password")
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name,"admin"),
+                     new Claim(ClaimTypes.Email,"admin@gmail.com")
+                };
+                var identity = new ClaimsIdentity(claims, "MyCookieAuth");
+                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
+
+                await HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal);
+                return Redirect("/Index");
+            }
+            return Page();
+        }
+    }
+}
